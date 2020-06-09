ba5819428d9357904f5f1fbc3602ac8513e1ee3b
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-06-09 16:30:20 +0900

    PriorityQueueを実装

---
+++ lib/priorityQueue_test.go
@@ -0,0 +1,85 @@
+package lib
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPriorityQueue(t *testing.T) {
+	type item struct {
+		node     *Node
+		priority int
+	}
+	tests := []struct {
+		Label    string
+		Input    []item
+		Expected []item
+	}{
+		{
+			Label: "SUCCESS: normal",
+			Input: []item{
+				{
+					node:     &Node{ID: 1, Name: "a", Links: []*Node{}},
+					priority: 10,
+				},
+				{
+					node:     &Node{ID: 2, Name: "b", Links: []*Node{}},
+					priority: 1,
+				},
+				{
+					node:     &Node{ID: 3, Name: "c", Links: []*Node{}},
+					priority: 5,
+				},
+			},
+			Expected: []item{
+				{
+					node:     &Node{ID: 1, Name: "a", Links: []*Node{}},
+					priority: 10,
+				},
+				{
+					node:     &Node{ID: 3, Name: "c", Links: []*Node{}},
+					priority: 5,
+				},
+				{
+					node:     &Node{ID: 2, Name: "b", Links: []*Node{}},
+					priority: 1,
+				},
+			},
+		},
+		{
+			Label: "Fial: pop is more than push",
+			Input: []item{
+				{
+					node:     &Node{ID: 1, Name: "a", Links: []*Node{}},
+					priority: 10,
+				},
+			},
+			Expected: []item{
+				{
+					node:     &Node{ID: 1, Name: "a", Links: []*Node{}},
+					priority: 10,
+				},
+				{
+					node:     nil,
+					priority: 0,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Label, func(t *testing.T) {
+			pq := InitPriorityQueue()
+			for _, input := range test.Input {
+				pq.Push(input.node, input.priority)
+			}
+
+			for _, expected := range test.Expected {
+				node, priority := pq.Pop()
+				assert.Equal(t, expected.node, node)
+				assert.Equal(t, expected.priority, priority)
+			}
+		})
+	}
+}
