2094e4f021531b83434bf999c090a9e2dcb9f7da
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-06-09 22:15:57 +0900

    wikipediaでのbfsを実装

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	fmt.Println("Select topic")
 	fmt.Println("1. SNS")
 	fmt.Println("2. Stations")
+	fmt.Println("3. Wikipedia")
 	fmt.Printf("> ")
 	input := lib.ReadLine()
 
@@ -22,6 +23,9 @@ func main() {
 		return
 	case "2":
 		topic.CmdStaions()
+		return
+	case "3":
+		topic.CmdWikipedia()
 		return
 	default:
 		fmt.Println("Invalid input")
---
+++ topic/wikipedita.go
@@ -0,0 +1,89 @@
+package topic
+
+import (
+	"fmt"
+
+	"github.com/oribe1115/step-graph/lib"
+	"github.com/oribe1115/step-graph/search"
+)
+
+type Wikipedia struct {
+	Graph *lib.Graph
+}
+
+func CmdWikipedia() {
+	fmt.Println("Initializing Wikipedia data...")
+	wikipedia, err := InitWikipedia()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Select mode with Wikipedia")
+	fmt.Println("1. Search Route by Breadth First Search")
+	fmt.Printf("> ")
+	input := lib.ReadLine()
+
+	switch input {
+	case "1":
+		fmt.Println("Input start name")
+		fmt.Printf("> ")
+		startName := lib.ReadLine()
+
+		fmt.Println("Input target name")
+		fmt.Printf("> ")
+		targetName := lib.ReadLine()
+
+		target, depth, route, err := wikipedia.BreadthFirstSearch(startName, targetName)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("target: %s, depth: %d\n", target.Sprint(), depth)
+		fmt.Printf("route: %s\n", lib.SprintNodeListAsRoute(route))
+		return
+	default:
+		fmt.Println("Invalid input")
+	}
+}
+
+func InitWikipedia() (*Wikipedia, error) {
+	wikipedia := &Wikipedia{}
+	wikipedia.Graph = lib.InitGraph()
+
+	nodeData, err := lib.ReadNodeData("./data/wikipedia/pages.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, nd := range nodeData {
+		node, err := lib.CreateNode(nd.ID, nd.Name)
+		if err != nil {
+			return nil, err
+		}
+
+		err = wikipedia.Graph.SetNode(node)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	linkData, err := lib.ReadLinkData("./data/wikipedia/links.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ld := range linkData {
+		err = wikipedia.Graph.LinkNodes(ld.FirstID, ld.SecondID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return wikipedia, nil
+}
+
+func (w *Wikipedia) BreadthFirstSearch(startName string, targetName string) (target *lib.Node, depth int, route []*lib.Node, err error) {
+	return search.BreadthFirstSearch(w.Graph, startName, targetName)
+}
